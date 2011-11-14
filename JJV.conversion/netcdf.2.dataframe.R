3f907c92bbda5ced74e92c8dd55cad57c781575d
jjvanderwal <jjvanderwal@c2dcb375-ba5a-08e5-3a40-df372bd21030>
2011-11-14 03:23:19 +0000

    added conversion script for netcdf to csv & esri ascii grid

---
+++ JJV.conversion/netcdf.2.dataframe.R
@@ -0,0 +1,37 @@
+#drafted by Jeremy VanDerWal ( jjvanderwal@gmail.com ... www.jjvanderwal.com )
+#GNU General Public License .. feel free to use / distribute ... no warranties
+
+################################################################################
+################################################################################
+# this is a request from Miranda Jones to convert climgen netcdf outputs into a 
+# csv file resembling...
+# Longitude,Latitude,SST
+# -179.75,89.75,-1.79
+# -179.25,89.75,-1.79
+# -178.75,89.75,-1.79
+# -178.25,89.75,-1.79
+# -177.75,89.75,-1.79
+# -177.25,89.75,-1.79
+################################################################################
+################################################################################
+library(ncdf); library(SDMTools) #load the necessary libraries
+
+wd = '~/tmp/conversion/'; setwd(wd);	#define and set the working directory
+nc = open.ncdf('climgen_output.nc')	#open a connection to the netcdf of interest
+	lon = get.var.ncdf(nc,'LONGITUDE')	#get a list of the longitudes
+    lat = get.var.ncdf(nc,'LATITUDE')	#get a list of the latitudes
+	month = get.var.ncdf(nc,'MONTH')	#get a list of the months
+	out = expand.grid(latitude=lat,longitude=lon)
+	
+	vars = names(nc$var)	#get a list of the data in the netcdf file
+	for (voi in vars) { cat(voi,'...\n') #cycle through each of hte variables
+		tdata = get.var.ncdf(nc,voi) #read in the data array for the variable of interses
+		for(ii in 1:length(month)) { out[,paste(voi,'_',sprintf('%02i',ii),sep='')] = as.vector(tdata[ii,,]) } #append the data to out
+	}
+close.ncdf(nc)	#close the netcdf file
+
+write.csv(out,'full.data.with.lat.lon.csv',row.names=FALSE)	#writewrite the csv... although I am writing out the full dataset, this can be subsetted
+dataframe2asc(out,gz=TRUE) #write out the individual columns as esri ascii grid files
+
+	
+
